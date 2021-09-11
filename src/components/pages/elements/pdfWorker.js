eebfc332418ff9b72bc6a6dae5589a8d0bec3cf3
Marcus Wills <marcuswills@Marcuss-MBP.eau.wi.charter.com>
2021-09-11 09:26:48 -0500

    bring in pdf worker locally

--- src/components/pages/elements/pdfWorker.js
+++ src/components/pages/elements/pdfWorker.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Worker } from '@react-pdf-viewer/core';
 import  PdfViewer  from './pdfViewer'
+import WorkerSrc from './pdf.worker'
 
 export default class pdfWorker extends React.Component {
     render (){
         return (
             <div>
-                <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js">
+                <Worker workerSrc={WorkerSrc}>
                     <PdfViewer pdfDir={this.props.pdfDir}/>
                 </Worker>
             </div>
